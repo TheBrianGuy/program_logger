f7c6daccd9ecf16358f3de4f2f7e8bb414922b85
Brian Hendrickson <brian.hendrickson@live.com>
2016-09-01 11:27:01 -0700

    working code

--- program_logger.rb
+++ program_logger.rb
@@ -1,19 +1,20 @@
-def log(block_description, some_proc)
-	puts "Beginning " + block_description + '"...'
+def log(block_description, &some_proc)
+	puts 'Beginning "' + block_description + '"...'
+	response = some_proc.call
 	puts '..."' + block_description + ' finished, returning:'
-	puts some_proc.call
+  puts response
 end
 
 log 'outer block' do
 	
 		log 'some little block' do
-			return 5
+			5
 		end
 
 		log 'yet another block' do
-			return "I like Thai food!"
+			"I like Thai food!"
 		end
 
-	return false
+	false
 end
 
